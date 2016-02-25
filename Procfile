d890707af12af7165b414ad2c82d057df44fde58
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-02-25 23:05:37 +0100

    Add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar  target/lepszy-klub-reservation-0.0.1-SNAPSHOT.jar
--- src/test/java/com/marcinmajkowski/lepszyklub/reservation/LepszyKlubReservationApplicationTests.java
+++ src/test/java/com/marcinmajkowski/lepszyklub/reservation/LepszyKlubReservationApplicationTests.java
@@ -1,11 +1,10 @@
 package com.marcinmajkowski.lepszyklub.reservation;
 
-import com.marcinmajkowski.lepszyklub.reservation.LepszyKlubReservationApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = LepszyKlubReservationApplication.class)
