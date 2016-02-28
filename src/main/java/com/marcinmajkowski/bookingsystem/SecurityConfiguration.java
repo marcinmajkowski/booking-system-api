dafb8329d6a2db2c14c7621092f8fba007bc8f56
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-02-29 00:31:03 +0100

    Add CSRF configuration

--- src/main/java/com/marcinmajkowski/bookingsystem/SecurityConfiguration.java
+++ src/main/java/com/marcinmajkowski/bookingsystem/SecurityConfiguration.java
@@ -1,18 +1,26 @@
 package com.marcinmajkowski.bookingsystem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
+import javax.servlet.Filter;
 import javax.sql.DataSource;
 
 @Profile("!dev")
 @Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -22,10 +30,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    @Qualifier("csrfHeaderFilter")
+    private Filter CsrfHeaderFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //TODO enable csrf
-        http.httpBasic().and().csrf().disable();
+        http
+                .httpBasic()
+                .and()
+                .addFilterAfter(CsrfHeaderFilter, CsrfFilter.class)
+                .csrf().csrfTokenRepository(csrfTokenRepository());
     }
 
     @Override
@@ -39,5 +54,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .inMemoryAuthentication()
                 .withUser("admin").password(adminPassword).roles("ADMIN", "USER");
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
     }
 }
