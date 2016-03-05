608e7b304da5833b22e753fb9978d4c90ed327a1
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-03-05 22:59:28 +0100

    Move front-end to separate application/repository

--- src/main/java/com/marcinmajkowski/bookingsystem/booking/BookingRepository.java
+++ src/main/java/com/marcinmajkowski/bookingsystem/booking/BookingRepository.java
@@ -2,7 +2,6 @@ package com.marcinmajkowski.bookingsystem.booking;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.validation.annotation.Validated;
 
 
 @RepositoryRestResource
