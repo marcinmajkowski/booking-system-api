7908dee0df14a95ef5a8c5333a59f78e52f2e29d
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-03-10 21:42:16 +0100

    Allow authenticated user to get his own Customer object by email

--- src/main/java/com/marcinmajkowski/bookingsystem/customer/CustomerRepository.java
+++ src/main/java/com/marcinmajkowski/bookingsystem/customer/CustomerRepository.java
@@ -1,10 +1,17 @@
 package com.marcinmajkowski.bookingsystem.customer;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
 @PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+
+    //TODO ignore case
+    @PreAuthorize("isAuthenticated()")
+    @PostAuthorize("returnObject?.email == principal.username or hasRole('ROLE_ADMIN')")
+    Customer findByEmail(@Param("email") String email);
 }
